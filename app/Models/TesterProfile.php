9b2304b41eef4efa29b9e007c6c5f28827d1610d
FattahulAlim <fathalim1512@gmail.com>
2026-04-20 11:13:14 +0700

    feat: implement tester profile creation and update user seeder emails

--- app/Models/TesterProfile.php
+++ app/Models/TesterProfile.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class TesterProfile extends Model
 {
     //
+    protected $fillable = [
+        'user_id',
+        'e_wallet_provider',
+        'e_wallet_number',
+        'points'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
